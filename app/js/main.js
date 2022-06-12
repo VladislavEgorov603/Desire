aae8436ca8cff911d4ec3bbd70a07d934ffb9d0b
Vladislav Egorov <vladislavegorov603@gmail.com>
2022-06-12 18:13:33 +0300

    0.0.2

--- app/js/main.js
+++ app/js/main.js
@@ -23,37 +23,47 @@ $(".contact__slider").slick({
     arrows: false,
 })
 
-document.querySelector(".header__button").addEventListener('click', function(){
-    document.querySelector(".header__drop-menu").classList.toggle("header__drop-menu_active");
+
+const images = $(".gallery__image");
+const viewBox = $(".gallery__view");
+const viewImage = $(".gallery__view-image");
+
+viewBox.on('click', function(){
+    viewImage.attr('src', "");
+    $(this).css('display', 'none')
 })
 
-var buttons = document.querySelectorAll(".gallery__menu-button");
+images.on('click', function(){
+    const imageLink = $(this).attr('src');
+    viewImage.attr('src', imageLink);
+    viewBox.css('display', 'block');
+})
 
-for(var button of buttons) {
-    button.addEventListener('click', function(){
-        buttons.forEach(i => i.setAttribute('class', 'gallery__menu-button gallery__menu-button_not-active'));
 
-        this.setAttribute('class', 'gallery__menu-button gallery__menu-button_active');
-    })
-};
+$(".header__button").on('click',function(){
+    $(".header__drop-menu").toggleClass('header__drop-menu_active');
+})
 
-var sidebarButtons = document.querySelectorAll(".sidebar__tags-item");
 
-for(var sidebarButton of sidebarButtons){
-    sidebarButton.addEventListener("click", function(){
-        this.classList.toggle('sidebar__tags-item_active')
-    })
-}
+$(".gallery__menu-button").on('click', function(){
+    $(".gallery__menu-button").attr('class', 'gallery__menu-button gallery__menu-button_not-active')
+    $(this).attr('class', 'gallery__menu-button gallery__menu-button_active')
+})
 
-var playButton = document.querySelector(".about__video-button");
-var videoPlayer = document.querySelector(".about__video-player");
+$(".sidebar__tags-item").on('click', function(){
+    $(this).toggleClass('sidebar__tags-item_active')
+})
 
-playButton.addEventListener('click', function(){
-    console.log("click");
+
+
+const videoPlayer = document.querySelector(".about__video-player");
+
+$('.about__video-button').on('click', function(){
     videoPlayer.play();
     videoPlayer.setAttribute('controls', 'controls');
-    this.setAttribute('hidden', 'true');
+    $(this).hide();
 })
+
 
 
 
